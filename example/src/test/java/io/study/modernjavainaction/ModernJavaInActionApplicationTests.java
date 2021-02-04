b678e403ba6e433f0eb20b9ebf523311647f67f9
gosgjung <playmymind@daum.net>
2021-02-04 21:37:12 +0900

    프로젝트 / 스터디문서 기본구조 커밋

---
+++ example/src/test/java/io/study/modernjavainaction/ModernJavaInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.modernjavainaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernJavaInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
