3656a005b1192052d2d367c5846b67fc51eeaa4a
soon.good <playmymind@daum.net>
2022-02-09 13:34:52 +0900

    예제 프로젝트 초기 구조 커밋

---
+++ JAVA-정주행/2022-JAVA-정주행-try1/javastudy2022ty1/src/main/java/io/study/lang/javastudy2022ty1/Javastudy2022ty1Application.java
@@ -0,0 +1,13 @@
+package io.study.lang.javastudy2022ty1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javastudy2022ty1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javastudy2022ty1Application.class, args);
+	}
+
+}
---
+++ JAVA-정주행/2022-JAVA-정주행-try1/javastudy2022ty1/src/test/java/io/study/lang/javastudy2022ty1/Javastudy2022ty1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.lang.javastudy2022ty1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javastudy2022ty1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
