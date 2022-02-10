055d42bcafe76172ff8cf9b804d135f2bdae1727
soon.good <playmymind@daum.net>
2022-02-11 02:24:02 +0900

    데이터 타입, 변수, 배열
    85번째 퍼즐 추가

---
+++ JAVA-정주행/2022-JAVA-정주행-try1/javastudy2022ty1/src/main/java/io/study/lang/javastudy2022ty1/puzzle86/Lazy.java
@@ -0,0 +1,28 @@
+package io.study.lang.javastudy2022ty1.puzzle86;
+
+public class Lazy {
+
+    public static boolean initialized = false;
+
+    static {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                initialized = true;
+            }
+        });
+
+        t.start();
+
+        try{
+            t.join();
+        }
+        catch (InterruptedException e){
+            throw new AssertionError(e);
+        }
+    }
+
+    public static void main(String [] args){
+        System.out.println(initialized);
+    }
+}
