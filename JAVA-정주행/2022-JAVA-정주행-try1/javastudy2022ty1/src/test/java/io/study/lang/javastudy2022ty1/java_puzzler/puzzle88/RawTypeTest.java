12e59be3a9a7d4921c64641d596c513ec278dfd9
soon.good <playmymind@daum.net>
2022-02-12 16:30:30 +0900

    퍼즐 88

---
+++ JAVA-정주행/2022-JAVA-정주행-try1/javastudy2022ty1/src/test/java/io/study/lang/javastudy2022ty1/java_puzzler/puzzle88/RawTypeTest.java
@@ -0,0 +1,13 @@
+package io.study.lang.javastudy2022ty1.java_puzzler.puzzle88;
+
+// 이건 변수/자료형 보다는 리플렉션/제너릭/애노테이션 정리할 때 정리하는 것이 나을듯해보인다.
+
+// 주요 개념
+// 원시타입(raw type) / 파라미터라이즈드 타입(parameterized type)
+// 클래스 리터럴, 자료형 토큰(type token)
+// ex)
+// = 원시타입 : List
+// = 파라미터라이즈드 타입 : List<Object>
+// 자바 5 이상에서는 원시 타입을 절대 사용하지 마세요
+public class RawTypeTest {
+}
