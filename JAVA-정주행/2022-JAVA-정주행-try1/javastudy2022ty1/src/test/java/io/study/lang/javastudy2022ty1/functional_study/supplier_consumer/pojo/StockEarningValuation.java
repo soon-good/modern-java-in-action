6a0c4046e8609b32b39251f54b7221db6adc546e
soon.good <playmymind@daum.net>
2022-06-07 04:39:48 +0900

    함수형 프로그래밍 정리중. = 예제용도 테스트 클래스 (StockEarningValuation)

---
+++ JAVA-정주행/2022-JAVA-정주행-try1/javastudy2022ty1/src/test/java/io/study/lang/javastudy2022ty1/functional_study/supplier_consumer/pojo/StockEarningValuation.java
@@ -0,0 +1,26 @@
+package io.study.lang.javastudy2022ty1.functional_study.supplier_consumer.pojo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Getter
+@ToString
+public class StockEarningValuation {
+    private final String ticker;
+    private final BigDecimal per;
+    private final BigDecimal forwardPer;
+    private final BigDecimal eps;
+
+    @Builder
+    public StockEarningValuation(
+        String ticker, BigDecimal per, BigDecimal forwardPer, BigDecimal eps
+    ){
+        this.ticker = ticker;
+        this.per = per;
+        this.forwardPer = forwardPer;
+        this.eps = eps;
+    }
+}
